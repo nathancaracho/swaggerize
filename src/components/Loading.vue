c9d7aa52f24b86ab3b4132da7bfb11a7befff833
Nathan Branco Caracho <nathan.caracho@hotmail.com>
2021-03-02 23:54:35 -0300

    adding load and copy

---
+++ src/components/Loading.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="spinner-Wrapper">
+    <svg class="spinner" viewBox="0 0 50 50">
+        <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5" stroke="#BBB"></circle>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style >
+  .spinner-Wrapper{
+    text-align: center;
+  }
+  .spinner{
+    margin: auto;
+    animation: rotate 2s linear infinite;
+    z-index: 2;
+    width: 50px;
+    height: 50px;
+  }
+  .spinner > .path{
+   
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+  }
+
+  @keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+  }
+
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
+  }
+
+</style>
+//based on
+//https://codepen.io/supah/pen/BjYLdW
